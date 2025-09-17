31c71a5016c7bb40c91ce1bff0516f2edc5b0486
harumaxy <harumaxy@gmail.com>
2025-09-18 01:01:38 +0900

    tweak useEditor hook

--- frontend/src/hooks/useEditor.ts
+++ frontend/src/hooks/useEditor.ts
@@ -13,11 +13,20 @@ interface EditorState {
   setEditText: (editText: string) => void;
 }
 
-export const useEditor = create<EditorState>((set) => ({
+export const useEditor = create<EditorState>((set, get) => ({
   selectedId: null,
   editTarget: null,
   editText: '',
-  select: (id: number | null) => set({ selectedId: id }),
+  select: (id: number | null) => {
+    const { selectedId: previousId, editTarget } = get();
+    if (id === previousId) return; // noop
+    // 別のコンテンツを選択したら、title or body の編集状態を解除
+    set({
+      selectedId: id,
+      editTarget: editTarget === 'list' ? editTarget : null,
+      editText: '',
+    });
+  },
   cancelSelect: () => set({ selectedId: null }),
   edit: (editTarget: EditTarget, initialText?: string) =>
     set({ editTarget, editText: initialText ?? '' }),
