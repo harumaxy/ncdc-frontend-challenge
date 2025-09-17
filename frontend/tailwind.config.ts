b8f08a3b77d9e24cc509c550d852eb8550a93ce3
harumaxy <harumaxy@gmail.com>
2025-09-17 13:12:15 +0900

    setup tailwind

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'bg-editor-section': '#F5F8FA',
+        'bg-list-hover': '#F5F8FA',
+        primary: '#32A8F8',
+      },
+      fontFamily: {
+        'noto-sans-jp': ['"Noto Sans JP"', 'sans-serif'],
+      },
+      borderRadius: {
+        sm: '4px',
+        md: '8px',
+        lg: '16px',
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
