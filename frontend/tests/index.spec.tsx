3cfa0aea2d5be3a46af11d2b58a684179b24724b
harumaxy <harumaxy@gmail.com>
2025-09-18 09:19:25 +0900

    add validation error test

--- frontend/tests/index.spec.tsx
+++ frontend/tests/index.spec.tsx
@@ -1,6 +1,9 @@
 import { expect } from '@playwright/test';
 import { test } from './utils';
 
+// E2E テストは、localhost:3000 でバックエンドサーバーが起動していることを前提とする
+// また、SQLite はコミットされている状態で初期化されていることを想定。更新があったら git checkout で戻しておく
+
 test('コンテンツを 作成 > 更新 > 削除 できることを確認する', async ({
   page,
 }) => {
@@ -29,7 +32,9 @@ test('コンテンツを 作成 > 更新 > 削除 できることを確認する
 
   // 本文を更新
   await page.getByTestId('body-edit-button').click();
-  await page.getByText('内容を入力してください').fill('編集された本文');
+  await page
+    .getByText('内容を入力してください')
+    .fill('編集された本文0123456789');
   await page.getByRole('button', { name: 'Save Save' }).click();
 
   // ページリロードしても更新内容が保持されていることを確認
@@ -49,7 +54,7 @@ test('コンテンツを 作成 > 更新 > 削除 できることを確認する
   await expect(
     page
       .locator('div')
-      .filter({ hasText: /^編集された本文$/ })
+      .filter({ hasText: /^編集された本文0123456789$/ })
       .first(),
   ).toBeVisible();
 
@@ -69,4 +74,40 @@ test('コンテンツを 作成 > 更新 > 削除 できることを確認する
       .locator('div')
       .filter({ hasText: /^編集されたタイトル$/ }),
   ).not.toBeVisible(); // 削除されたコンテンツがサイドバーから消える
+});
+
+test('タイトル・本文の編集フォームに制限文字数より多い/少ない文字数が入力されているときに、エラーメッセージが表示される', async ({
+  page,
+}) => {
+  await page.goto('/');
+  await page
+    .locator('div')
+    .filter({ hasText: /^方丈記$/ })
+    .click();
+  const titleErrorMessage = page.getByText(
+    'タイトルは1文字以上、50文字以下である必要があります',
+  );
+  await page.getByTestId('title-edit-button').click();
+  await page.getByRole('textbox').fill('');
+  await expect(titleErrorMessage).toBeVisible();
+  await page.getByRole('textbox').fill('あ');
+  await expect(titleErrorMessage).not.toBeVisible();
+  await page.getByRole('textbox').fill('あ'.repeat(50));
+  await expect(titleErrorMessage).not.toBeVisible();
+  await page.getByRole('textbox').fill('あ'.repeat(51));
+  await expect(titleErrorMessage).toBeVisible();
+
+  const bodyErrorMessage = page.getByText(
+    '本文は10文字以上、2000文字以下である必要があります',
+  );
+  await page.getByRole('button', { name: 'Cancel Cancel' }).click();
+  await page.getByTestId('body-edit-button').click();
+  await page.getByRole('textbox').fill('あ'.repeat(9));
+  await expect(bodyErrorMessage).toBeVisible();
+  await page.getByRole('textbox').fill('あ'.repeat(10));
+  await expect(bodyErrorMessage).not.toBeVisible();
+  await page.getByRole('textbox').fill('あ'.repeat(2000));
+  await expect(bodyErrorMessage).not.toBeVisible();
+  await page.getByRole('textbox').fill('あ'.repeat(2001));
+  await expect(bodyErrorMessage).toBeVisible();
 });
